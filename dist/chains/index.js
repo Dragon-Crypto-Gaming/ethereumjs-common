f866e52bcbe6ce23270e7b18d131e99d5162d6b4
PolyPup <polypup.farm@gmail.com>
2023-10-10 22:52:48 -0400

    add dist folder

---
+++ dist/chains/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.chains = void 0;
+exports.chains = {
+    names: {
+        '1': 'mainnet',
+        '3': 'ropsten',
+        '4': 'rinkeby',
+        '42': 'kovan',
+        '6284': 'goerli',
+        '43113': 'fuji',
+        '43114': 'avalanche',
+    },
+    mainnet: require('./mainnet.json'),
+    ropsten: require('./ropsten.json'),
+    rinkeby: require('./rinkeby.json'),
+    kovan: require('./kovan.json'),
+    goerli: require('./goerli.json'),
+    fuji: require('./fuji.json'),
+    avalanche: require('./avalanche.json'),
+};
+//# sourceMappingURL=index.js.map
